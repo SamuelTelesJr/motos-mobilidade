675ec0dee93c5b43fa508d31ca4aa502884ba6d5
SamuelTelesJr <samueltelesjunior17@gmail.com>
2025-07-04 23:11:23 -0300

    Remove binary assets and references

---
+++ app/app/(tabs)/index.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Motos Mobilidade</ThemedText>
+      <ThemedText>Aplicativo em React Native usando Expo.</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
+  },
+});
